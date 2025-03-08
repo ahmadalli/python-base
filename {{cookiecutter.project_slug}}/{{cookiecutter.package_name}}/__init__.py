814bb28941b869f0b9f4a6d47224c138d677e98e
ahmadali shafiee <mail@ahmadalli.net>
2025-03-08 20:45:16 +0100

    Fix formatting

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/__init__.py
@@ -6,8 +6,7 @@ __version__ = "0.1.0"
 
 # Set up logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 # Create a logger for this package
